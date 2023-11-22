3e06a1db1b781cab3312a4860114028b4ab999a5
loupdemon <azzougaghiles@hotmail.com>
2023-11-22 11:20:30 +0100

    first code

---
+++ first_projet_vava/src/MyFirstAPP.java
@@ -0,0 +1,12 @@
+//une classe commence toujours par majuscule et contient le nom du fichier
+//pour définrir une classe il faut un modificateur d'accès le x(public ou private ...) + class + Nom
+public class MyFirstAPP {
+     //la méthode rend les opration connecté, dans d'autres language appeler procedures ou exécussion
+    //public rend la classe ouverte et accessible par d'autre code
+    //public =/= Public  static = montre que l'objet appartient à class et no à l'objet crée par la classe
+    //voici la méthodee main, on peut exécuter la methode de n'importe quel endroit different
+    public static void main(String[] arg){ //un tableau , args nom de la liste
+        System.out.println("Hello World");// system : une class integré dans java, out champs pour montrer une sortie, println sur ecran sur une seule ligne
+
+    }
+}
