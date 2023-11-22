4af1f2b7b86c40abc9794c33976b12bc31028165
loupdemon <azzougaghiles@hotmail.com>
2023-11-22 13:14:47 +0100

    variable et conversion et objets

--- first_projet_vava/src/MyFirstAPP.java
+++ first_projet_vava/src/MyFirstAPP.java
@@ -6,7 +6,38 @@ public class MyFirstAPP {
     //public =/= Public  static = montre que l'objet appartient à class et no à l'objet crée par la classe
     //voici la méthodee main, on peut exécuter la methode de n'importe quel endroit different
     public static void main(String[] arg){ //un tableau , args nom de la liste
-        System.out.println("Hello World");// system : une class integré dans java, out champs pour montrer une sortie, println sur ecran sur une seule ligne
 
+        //First_code Branch
+
+        //System.out.println("Hello World");// system : une class integré dans java, out champs pour montrer une sortie, println sur ecran sur une seule ligne //
+
+        short puissance = 362;
+        short emissionCo2 = 333;
+        short cylindree = 6714;
+        short place = 5;
+        int prix = 36000;
+        int kilometrage = 10800;
+
+        long numeromatriculation = 123456789L;
+
+        float consommatonRoute = 15.5F;
+        float consommationVille = 13.6F;
+
+        double consommationMoyenne = 151.564789123-1;
+
+        boolean estEndomagee = true;
+
+        char classEnergeique ='F';
+
+        System.out.println("dodge Challenger SRT");
+        System.out.println("Prix : $" + prix+" Immatriculation" + numeromatriculation);
+
+
+        System.out.println("Conversion");
+        System.out.println("NvKilometrage" + kilometrage); //elargissement
+        int nvKilometrage = place;
+        System.out.println("le kilometrage nouveau" + nvKilometrage);
+        byte nvPrix=(byte)prix;
+        System.out.println("le prix nouveau" + nvPrix); //retricissement errro car trop grand
     }
 }
