04668128c28f95f927cd3aaeffef817b8e65da94
loupdemon <azzougaghiles@hotmail.com>
2023-11-23 14:15:03 +0100

    programme distributeur avec promotion

--- first_projet_vava/src/SwitchApp.java
+++ first_projet_vava/src/SwitchApp.java
@@ -1,127 +1,150 @@
 import java.util.Scanner;
 
 public class SwitchApp {
+    // Utilisez un seul Scanner pour tout le programme
+    private static Scanner scanner = new Scanner(System.in);
+    // Prix des boissons
+    private static double prixCoca = 2.5;
+    private static double prixPepsi = 3.0;
+    private static double prixFanta = 2.8;
+    private static double prixSprite = 2.7;
+
+    private static int limiteTentantveCode = 3;
+
     public static void main(String[] args) {
-        choixBoisson();
+        System.out.println("Voici les boissons");
         combienFoisEcouterReponseVocale();
-        activerPromotion();
+        choixBoisson();
     }
 
-
-
-    // les méthodes statiques peuvent être appelées par des méthodes statiques
+    // Les méthodes statiques peuvent être appelées par des méthodes statiques
     static void choixBoisson() {
-        System.out.println("QUELLE choix de boisson veux-tu?");
+        System.out.println("Quelle boisson veux-tu?");
+        listBoisson();
+        System.out.println("Fais un choix en entrant le chiffre");
+        int boisson = scanner.nextInt();
+        System.out.println("Vous avez saisi : " + boisson);
+
+        switch (boisson) {
+            case 1:
+                System.out.println("Vous avez choisi Coca.");
+                activerPromotion(prixCoca);
+                break;
+            case 2:
+                System.out.println("Vous avez choisi Pepsi.");
+                activerPromotion(prixPepsi);
+                break;
+            case 3:
+                System.out.println("Vous avez choisi Fanta.");
+                activerPromotion(prixFanta);
+                break;
+            case 4:
+                System.out.println("Vous avez choisi Sprite.");
+                activerPromotion(prixSprite);
+                break;
+            default:
+                System.out.println("Choix invalide. Veuillez choisir un numéro de boisson valide.");
+                choixBoisson();
+        }
     }
-
-
 
     static void listBoisson() {
-        System.out.println("1/ coca");
-        System.out.println("2/ pepsi");
-        System.out.println("3/ fanta");
-        System.out.println("4/ sprite");
+        System.out.println("1/ Coca - " + prixCoca + "€");
+        System.out.println("2/ Pepsi - " + prixPepsi + "€");
+        System.out.println("3/ Fanta - " + prixFanta + "€");
+        System.out.println("4/ Sprite - " + prixSprite + "€");
     }
-
-
-
 
     static void combienFoisEcouterReponseVocale() {
-        // Créer un objet Scanner pour lire l'entrée de la console
-        Scanner scanner = new Scanner(System.in);
-
         System.out.println("Combien de fois veux-tu écouter le guide vocal?");
-
-        // Demander à l'utilisateur d'entrer quelque chose
         System.out.print("Veuillez entrer le nombre en chiffre : ");
-
-        // Lire l'entrée de l'utilisateur
-        int userInput = scanner.nextInt();  // Utilisez nextInt() pour lire un entier
-
-        // Afficher ce que l'utilisateur a saisi
+        int userInput = scanner.nextInt();
         System.out.println("Vous avez saisi : " + userInput);
-
-        // Fermer le scanner pour éviter les fuites de ressources
         repeterReponse(userInput);
-        scanner.close();
     }
 
-    static void activerPromotion() {
-        // Créer un objet Scanner pour lire l'entrée de la console
-        Scanner scanner = new Scanner(System.in);
-
+    static void activerPromotion(double prixBoisson) {
         System.out.println("Avez-vous un code-promo?");
-        //boolean reponseOuiOuNon = scanner.nextBoolean(); // lire un boolean
-        //boolean reponseOuiOuNon = Boolean.parseBoolean(scanner.next());
-
         String reponse = scanner.next().toLowerCase();
+
+        while (!reponse.equals("oui") && !reponse.equals("non")) {
+            System.out.println("Nous n'avons pas compris votre réponse, répondez juste par Oui ou Non");
+            reponse = scanner.next().toLowerCase();
+        }
         boolean reponseOuiOuNon = reponse.equals("oui");
-
-
         if (reponseOuiOuNon) {
-            // Demander à l'utilisateur d'entrer quelque chose
-            System.out.print("Veuillez entrer le code : ");
-            String entreUser = scanner.next(); // Utilisez next() pour lire une chaîne
+            // Valider le code promo avant le switch
+            quellePromo(prixBoisson);
+        }else System.out.println("vous n'allez pas bénéficier de réduction");
 
-            // Afficher ce que l'utilisateur a saisi
-            System.out.println("Vous avez saisi : " + entreUser);
 
-            // Valider le code promo (par exemple, vérifier la longueur minimale)
-            if (validerCodePromo(entreUser)) {
-                switch (entreUser.toLowerCase()) {
-                    case "abc1":
-                        System.out.println("On activera la promotion forte");
-                        break;
+    }
 
-                    case "def2":
-                        System.out.println("On activera la promotion moyenne");
-                        break;
+    static void quellePromo( double prixBoisson){
+        System.out.print("Veuillez entrer le code : ");
+        String entreUser = scanner.next();
+        System.out.println("Vous avez saisi : " + entreUser);
+        limiteTentantveCode--;
 
-                    default:
-                        System.out.println("Votre code ne fonctionne pas");
-                }
-            } else {
-                System.out.println("Le code promo n'est pas valide.");
+        if (validerCodePromo(entreUser)) {
+            double nouveauPrix;
+
+            switch (entreUser) {
+                case "abc1":
+                    System.out.println("On activera la promotion forte");
+                    nouveauPrix = activePromotionDuPrixFort(prixBoisson);
+                    System.out.println("Voila ce que vous allez payer : " + nouveauPrix + "€");
+                    break;
+                case "def2":
+                    System.out.println("On activera la promotion faible");
+                    nouveauPrix = activePromotionDuPrixFaible(prixBoisson);
+                    System.out.println("Voila ce que vous allez payer : " + nouveauPrix + "€");
+                    break;
+                default:
+                    System.out.println("il reste"+ limiteTentantveCode);
+
+                    if(limiteTentantveCode>0) {
+                        System.out.println("Votre code ne fonctionne pas réessayez !");
+                        System.out.println("Il vous reste "+limiteTentantveCode+" tentative");
+                        //return;
+                        quellePromo(prixBoisson);
+                    }else System.out.println("vous avez épuisez toutes vos tentatives, reprenez de zéro");
             }
+        } else {
+            System.out.println("Le code promo n'est pas valide.");
+            if(limiteTentantveCode>0) {
+                System.out.println("Votre code doit avoir que 4 caractere!");
+                System.out.println("Il vous reste "+limiteTentantveCode+" tentative");
+                //return;
+                quellePromo(prixBoisson);
+            }else System.out.println("vous avez épuisez toutes vos tentatives, reprenez de zéro");
         }
 
-        // Fermer le scanner pour éviter les fuites de ressources
-        scanner.close();
     }
 
     static boolean validerCodePromo(String code) {
-        // Ajoutez ici votre logique de validation du code promo
-        // Par exemple, vérifiez la longueur minimale ou d'autres critères
-        return code.length() >= 4; // Exemple : Le code doit avoir une longueur minimale de 4 caractères
+        return code.length() >= 4;
     }
 
     static void repeterReponse(int repeterChoix) {
         for (int i = 1; i <= repeterChoix; i++) {
             listBoisson();
-            if(i<repeterChoix){
-                int nombreFoisQuiReste = repeterChoix -i;
-                if(nombreFoisQuiReste ==1){
+            if (i < repeterChoix) {
+                int nombreFoisQuiReste = repeterChoix - i;
+                if (nombreFoisQuiReste == 1) {
                     System.out.print("Voici un dernier rappel de la liste des boissons");
-
                 }
-                System.out.println("je vais repeter à nouveau la liste des boissons pour" + nombreFoisQuiReste+ " fois  veuillez bien écouter ");
-
+                System.out.println("Je vais répéter à nouveau la liste des boissons pour " + nombreFoisQuiReste + " fois. Veuillez bien écouter.");
             }
         }
     }
 
-
-
-
-
-
-    static double getPromotionDuPrixMoyen(int prix){
-        return prix *0.75;
+    static double activePromotionDuPrixFaible(double prix) {
+        return prix * 0.75;
     }
 
-    static double getPromotionDuPrixFort(int prix){
-        return prix *0.5;
+    static double activePromotionDuPrixFort(double prix) {
+        return prix * 0.5;
     }
-
 
 }
